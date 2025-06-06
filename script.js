b9f8ad5daf19b841ce3f1b63af8e4250f6f2285f
wiping_ik <ichikawa@wiping.co.jp>
2025-06-06 11:05:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const video = document.getElementById('mainVideo');
 const choices = document.getElementById('choices');
 
 video.addEventListener('timeupdate', () => {
+  // 再生時間が3秒以上で、まだ選択肢が表示されていない場合
   if (video.currentTime >= 3 && choices.style.display === 'none') {
-    choices.style.display = 'block';
+    choices.style.display = 'flex'; // display:block ではなく flex に変更
   }
 });
 
